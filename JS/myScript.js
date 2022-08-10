b44c6c34f641453d01a59cc0a7c957bb53ae2bdc
Aeuron <garrymcgee96@gmail.com>
2022-08-09 19:36:18 -0700

    bmi

--- JS/myScript.js
+++ JS/myScript.js
@@ -69,3 +69,27 @@
 // function bmiCalculator(weight, height){
 //     return weight / (height * height);
 // }
+
+// function bmiCalculator (weight, height) {
+//
+//     var bmi = weight / (height * height);
+//     var interpretation = "";
+//
+//     if(bmi < 18.5)
+//     {
+//         interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+//         console.log(interpretation);
+//     }
+//     else if(bmi > 18.5 && bmi < 24.9)
+//     {
+//         interpretation = "Your BMI is " + bmi + ", so you have a normal weight.";
+//         console.log(interpretation);
+//     }
+//     else if(bmi > 24.9)
+//     {
+//         interpretation = "Your BMI is " + bmi + ", so you are overweight.";
+//         console.log(interpretation);
+//     }
+//
+//     return interpretation;
+// }
