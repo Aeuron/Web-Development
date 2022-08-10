cddd4aa632804842bd891bc4a3021958d1116688
idontknowgarry <garrymcgee96@gmail.com>
2022-08-09 17:35:03 -0700

    javascript

---
+++ JS/myScript.js
@@ -0,0 +1,48 @@
+// var variable = 300 + -50;
+// var variable = "300 + -50";
+// var variable = true;
+// alert(typeof(variable));
+
+// var name = prompt("What is your name?");
+// var user_name = "Garry";
+// alert(user_name);
+
+// var begin = "Start ";
+// var end = "here.";
+
+// var message = prompt("Say something.");
+// var messageCount = message.slice(0,140);
+
+// if(message.length > 140)
+// {
+//     alert("Too many characters.")
+// }
+// else if(message.length <= 140)
+// {
+//     alert("You have said " + message.length + " characters and have " + (140 - message.length) + " characters remaining.");
+// }
+
+// messageCount;
+// messageCount.length;
+
+// var first_name = "Garry";
+// var last_name = "mcgee";
+// first_name.toUpperCase();
+// last_name.toLowerCase();
+
+// Name input
+// var name = prompt("Name?");
+// var firstChar = name.slice(0,1);
+// var upperCaseFirstChar = firstChar.toUpperCase();
+
+// var restOfName = name.slice(1, name.length);
+// var lowerCaseRestOfName = restOfName.toLowerCase();
+
+// alert("Hello, " + upperCaseFirstChar + lowerCaseRestOfName);
+
+// Dog Age Converter
+var dogAge = 18;
+
+var humanAge = ((dogAge - 2) * 4) + 21
+
+humanAge;
